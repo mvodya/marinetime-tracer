f618443c224d109a774fb87f0bcc80daba692764
Mark Vodyanitskiy <mvodya@icloud.com>
2024-11-01 10:21:45 +0000

    Added watchdog to script

--- mt-collector-bot/script.sh
+++ mt-collector-bot/script.sh
@@ -4,7 +4,8 @@ source /workspace/venv/bin/activate
 
 trap "echo 'Parse stopped'; exit" SIGINT
 
-python collector.py
+# 25 min watchdog
+timeout 1500 python collector.py
 
 if [ $? -eq 0 ]; then
     timestamp=$(date +"%d.%m.%Y_%H_%M")
@@ -15,6 +16,8 @@ if [ $? -eq 0 ]; then
     echo "Data copied to archive/${timestamp}.json"
 
     python upload.py
+elif [ $? -eq 124 ]; then
+    echo "collector.py timed out after 25 minutes"
 else
     echo "collector.py failed, skipping upload.py"
 fi
